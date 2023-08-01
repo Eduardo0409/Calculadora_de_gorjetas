e4f5d276159b704a70345c1e90cd01a289cb17c7
Eduardo Alves <du.caldas.alves@gmail.com>
2023-08-01 18:33:11 -0300

    Javascript finalizado

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-//Funcao para calcular a gorjeta
+// Funcao para calcular a gorjeta
 function calculateTip(event){
     event.preventDefault();
     let account = document.getElementById("account").value;
@@ -10,13 +10,27 @@ function calculateTip(event){
         alert("Por favor, preencha os valores.")
         return;
     }
+
+    // If para mostrar ou nao a mensagem para o numero de pessoas
+    if(people == '' | people <= 1){
+        people = 1;
+        document.getElementById("each").style.display = "none"
+    } else{
+        document.getElementById("each").style.display = "block"
+    }
+
+    // Calculo da gorjeta
+    let total = (account * service) / people;
+    total = total.toFixed(2);
+    document.getElementById("tip").innerHTML = total;
+    document.getElementById("totalTip").style.display = "block";
 }
 
 
-//Document para ocultar os elementos
+// Document para ocultar os elementos
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";
 
 
-//Evento para invocar a funcao 
+// Evento para invocar a funcao 
 document.getElementById("tipsForm").addEventListener('submit', calculateTip);
